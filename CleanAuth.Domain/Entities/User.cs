5f42cad5bf609a225ab78abf569874697295924f
AbdurehmanXX <akhondak84@gmail.com>
2026-08-02 21:51:12 +0500

    Add project files.

---
+++ CleanAuth.Domain/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using CleanAuth.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanAuth.Domain.Entities
+{
+    public class User: BaseEntity
+    {
+        [Required]
+        public string FirstName { get; set; }= string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string PasswordHash { get; set; }= string.Empty;
+        public bool IsEmailVerified { get; set; } = false;
+        public bool  IsActive { get; set; }
+    }
+}
---
+++ CleanAuth.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using CleanAuth.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanAuth.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure
+            (this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDBContext>
+                (options => options.UseSqlServer
+                (configuration.GetConnectionString("DefaultConnection")));
+            return services;
+        }
+    }
+}
